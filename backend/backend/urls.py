6edef352f8645f5f6a323495c73971170a4fcc8e
Aleksey Korshuk <ale-kor02@mail.ru>
2024-10-03 22:00:43 -0700

    first commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# keep all roots related to authentication & registration in the same place
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include("api.urls")), # forward specific URLs to this urls.py to be handled from this app
+]
+
+
---
+++ frontend/src/constants.js
@@ -0,0 +1,3 @@
+//use local storage to store access and refresh token in browser, so the following const are keys to access the access token and refresh token
+export const ACCESS_TOKEN = "access"
+export const REFRESH_TOKEN = "refresh"
