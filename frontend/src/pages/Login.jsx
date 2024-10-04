4a168c46bb58605e9e09447c0b0bfc7cff11289b
Aleksey Korshuk <ale-kor02@mail.ru>
2024-10-04 03:26:57 -0700

    edited login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,21 @@
-import Form from "../components/Form"
+import Form from "../components/Form";
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return (
+        <div>
+            <p>
+                Check out the source code on{" "}
+                <a
+                    href="https://github.com/sruthy16/notes-manager-aggieworks-takehome"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>.
+            </p>
+            <Form route="/api/token/" method="login" />
+        </div>
+    );
 }
 
-export default Login
+export default Login;
